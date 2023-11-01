aab0f4f2bf9952a454dc8720f61e770044149070
stevenorum <stevenorum@gmail.com>
2023-11-01 13:23:53 -0400

    Allow WotC in footer lines.

--- js/creator-23.js
+++ js/creator-23.js
@@ -1703,7 +1703,8 @@ async function bottomInfoEdited() {
 	card.infoArtist = document.querySelector('#info-artist').value;
 	card.infoYear = document.querySelector('#info-year').value;
 	for (var textObject of Object.entries(card.bottomInfo)) {
-		if (["NOT FOR SALE", "Wizards of the Coast", "CardConjurer.com", "cardconjurer.com"].some(v => textObject[1].text.includes(v))) {
+		// if (["NOT FOR SALE", "Wizards of the Coast", "CardConjurer.com", "cardconjurer.com"].some(v => textObject[1].text.includes(v))) {
+		if (["NOT FOR SALE", "CardConjurer.com", "cardconjurer.com"].some(v => textObject[1].text.includes(v))) {
 			continue;
 		} else {
 			await writeText(textObject[1], bottomInfoContext);
